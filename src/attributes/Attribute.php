c4263efd59c835db090f37d95f2792a8ad8d9536
Jefferson Silva <jeffersonprintsilva@gmail.com>
2023-05-29 23:18:02 -0300

    update Attribute.php , add CheckAttribute.php

--- src/attributes/Attribute.php
+++ src/attributes/Attribute.php
@@ -1,20 +1,57 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Outline\Plain\Html\Attributes;
 
 use Outline\Plain\Html\Core\Abstraction\AbstractAttribute;
 use Outline\Plain\Html\Core\Enumeration\EnumAttribute;
 use Outline\Plain\Html\Core\Enumeration\EnumInput;
 
+/**
+ * @author Jefferson Silva Santos
+ */
 class Attribute extends AbstractAttribute
 {
-    public function __construct(EnumAttribute $attribute, string|EnumInput $value)
+    use CheckAttribute;
+
+    /**
+     * @var string
+     */
+    private string $attribute_name;
+
+    /**
+     * @var string
+     */
+    private string $attribute_value;
+
+    /**
+     * @param EnumAttribute $attribute
+     * @param string|int|bool|EnumInput $value
+     */
+    public function __construct(EnumAttribute $attribute, string|int|bool|EnumInput $value)
     {
-        parent::__construct($attribute, $value);
+        $this->config_attribute($attribute,$value);
+        parent::__construct($attribute, $this->attribute_value);
     }
 
+    /**
+     * @param EnumAttribute $attribute
+     * @param string|int|bool|EnumInput $value
+     * @return void
+     */
+    private function config_attribute(EnumAttribute $attribute, string|int|bool|EnumInput $value): void
+    {
+        $this->attribute_name = $attribute->value;
+        $this->attribute_value = $this->filter_value($attribute, $value);
+    }
+
+    /**
+     * @return string
+     */
     public function attribute(): string
     {
+        if (!$this->receive_value($this->attribute_name)) return $this->attribute_name;
         return "";
     }
 }
---
+++ src/attributes/CheckAttribute.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Outline\Plain\Html\Attributes;
+
+use Outline\Plain\Html\Core\Enumeration\EnumAttribute;
+use Outline\Plain\Html\Core\Enumeration\EnumInput;
+
+/**
+ * @author Jefferson Silva Santos
+ */
+trait CheckAttribute
+{
+    /**
+     * @param string $attribute
+     * @return bool
+     */
+    protected function receive_value(string $attribute): bool
+    {
+        $list =  [
+            EnumAttribute::ATT_TYPE->value,
+            EnumAttribute::ATT_NAME->value,
+            EnumAttribute::ATT_SIZE->value,
+            EnumAttribute::ATT_FOR->value,
+            EnumAttribute::ATT_MAX->value,
+            EnumAttribute::ATT_MAXLENGTH->value,
+            EnumAttribute::ATT_MIN->value,
+            EnumAttribute::ATT_MINLENGTH->value,
+            EnumAttribute::GLOBAL_ATT_CLASS->value,
+            EnumAttribute::GLOBAL_ATT_ID->value,
+            EnumAttribute::ATT_AUTOCOMPLETE->value,
+            EnumAttribute::ATT_PLACEHOLDER->value,
+            EnumAttribute::ATT_DIRNAME->value,
+            EnumAttribute::ATT_LIST->value,
+            EnumAttribute::ATT_VALUE->value,
+            EnumAttribute::ATT_PATTERN->value,
+        ];
+        return in_array($attribute, $list);
+    }
+
+    /**
+     * @param EnumAttribute $attribute
+     * @param string|int|bool|EnumInput $value
+     * @return string
+     */
+    protected function filter_value(EnumAttribute $attribute, string|int|bool|EnumInput $value): string
+    {
+        return match ($attribute->value) {
+            EnumAttribute::ATT_TYPE->value => $value instanceof EnumInput ? $value->value : $value,
+            default => $value
+        };
+    }
+
+
+
+
+}
